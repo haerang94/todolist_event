7969029e02bccb27956be42de146ad7dd37357f3
haerangChoi <gofkdvjvl@naver.com>
2021-02-09 20:05:27 +0900

    feat: 드롭다운 구현 및 스타일링

--- calendar.js
+++ calendar.js
@@ -1,3 +1,6 @@
+const leap = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+const plain = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
 //  윤년인지 체크
 const isLeapYear = () => {
   // 윤년인 경우 true, 평년인 경우 false
@@ -9,4 +12,21 @@ const isLeapYear = () => {
     return true;
   }
   return false;
+};
+
+const untilLastYear = (year) => {
+  let tot = 0;
+  for (let i = 1; i < year; i++) {
+    if (isLeapYear(i)) tot += 366;
+    else tot += 365;
+  }
+  return tot;
+};
+
+const untilLastMonth = (year, month) => {
+  let tot = untilLastYear(year);
+  for (let i = 1; i < month; i++) {
+    if (isLeapYear(year)) tot += leap[i - 1];
+    else tot += plain[i - 1];
+  }
 };
