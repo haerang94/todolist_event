2446f9efa7e4ab2c5f9515d9b0a402ce5b42dae6
haerangChoi <gofkdvjvl@naver.com>
2021-02-10 01:49:32 +0900

    feat: 달력 만듬:

--- calendar.js
+++ calendar.js
@@ -1,3 +1,18 @@
+let yearContent = document.getElementById("year");
+const monthContent = document.querySelector(".select");
+const dropDown = document.getElementById("month");
+const date = document.getElementById("date");
+
+const today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth() + 1;
+var yyyy = today.getFullYear();
+yearContent.textContent = yyyy;
+monthContent.textContent = `${mm}월`;
+let year = yyyy;
+let month = mm;
+console.log(dd);
+
 const leap = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 const plain = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
@@ -29,4 +44,46 @@ const untilLastMonth = (year, month) => {
     if (isLeapYear(year)) tot += leap[i - 1];
     else tot += plain[i - 1];
   }
+  return tot;
 };
+
+const untilYesterday = (year, month, day) => {
+  let tot = untilLastMonth(year, month);
+  return tot + day;
+};
+
+const lastDay = (year, month) => {
+  let day = 0;
+  if (isLeapYear(year)) {
+    day = leap[month - 1];
+  } else {
+    day = plain[month - 1];
+  }
+  return day;
+};
+console.log(
+  yearContent.textContent,
+  monthContent.textContent.substring(0, monthContent.textContent.length - 1)
+);
+
+const insertDays = () => {
+  let firstDay = untilLastMonth(year, month) % 7;
+  let last = lastDay(year, month);
+  let template = "";
+  console.log(untilLastMonth(year, month), firstDay);
+  for (let i = 0; i <= firstDay; i++) {
+    template += `<div class="day"></div>`;
+  }
+
+  for (let i = 1; i <= last; i++) {
+    if (i === dd) {
+      template += `<div class="day today">${i}</div>`;
+    } else {
+      template += `<div class="day">${i}</div>`;
+    }
+  }
+  console.log(date, template);
+  date.innerHTML = template;
+};
+
+insertDays();
