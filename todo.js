d82d30432c8d321071a89fe3422ce12a7eadca72
haerangChoi <gofkdvjvl@naver.com>
2021-02-15 00:06:07 +0900

    feat: localStorage에 저장, 추가, 삭제 함수 구현

--- todo.js
+++ todo.js
@@ -11,26 +11,76 @@ const todoSearchBtn = document.getElementById("todo-search-btn");
 const doneSearchBtn = document.getElementById("done-search-btn");
 let isTodo = false;
 let isDone = false;
-
+let todos;
+let dones;
 const background = ["#f1f6f9"];
 const label = ["#14274e"];
-// 투두리스트 추가
-const addTodo = () => {
-  if (inputContent.value === "") {
-    alert("write a task");
-    return;
-  }
+
+// 투두리스트 저장
+const storeTodo = (todo) => {
+  todos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
+const storeDone = (done) => {
+  dones.push(done);
+  localStorage.setItem("dones", JSON.stringify(dones));
+};
+// ls에서 삭제
+const removeTodoFromLS = (deleteItem) => {
+  // todo:수정 필요. 같은 투두리스트의 경우 둘다 지워짐
+  todos.forEach((todo, idx) => {
+    if (deleteItem.textContent === todo) {
+      todos.splice(idx, 1);
+    }
+  });
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
+const removeDoneFromLS = (deleteItem) => {
+  // todo:수정 필요. 같은 투두리스트의 경우 둘다 지워짐
+  dones.forEach((done, idx) => {
+    if (deleteItem.textContent === done) {
+      dones.splice(idx, 1);
+    }
+  });
+  localStorage.setItem("dones", JSON.stringify(dones));
+};
+// 재사용성 위해: todo or inputContent.value
+const makeTodolist = (todo = inputContent.value) => {
   const wrapper = document.createElement("div");
   wrapper.className = "todo-flexbox";
   const newTodo = document.createElement("div");
   newTodo.className = "todo-item";
-  newTodo.appendChild(document.createTextNode(inputContent.value));
+  newTodo.appendChild(document.createTextNode(todo));
   wrapper.appendChild(newTodo);
   const link = document.createElement("a");
   link.className = "delete-todo";
   link.innerHTML = `<i class="far fa-trash-alt"></i>`;
   wrapper.appendChild(link);
   todoList.appendChild(wrapper);
+};
+
+const makeDonelist = (done = inputContent.value) => {
+  const wrapper = document.createElement("div");
+  wrapper.className = "done-flexbox";
+  const newTodo = document.createElement("div");
+  newTodo.className = "done-item";
+  newTodo.appendChild(document.createTextNode(done));
+  wrapper.appendChild(newTodo);
+  const link = document.createElement("a");
+  link.className = "delete-done";
+  link.innerHTML = `<i class="far fa-trash-alt"></i>`;
+  wrapper.appendChild(link);
+  doneList.appendChild(wrapper);
+};
+
+// 투두리스트 추가
+const addTodo = () => {
+  if (inputContent.value === "") {
+    alert("write a task");
+    return;
+  }
+  makeTodolist();
+  storeTodo(inputContent.value);
   inputContent.value = "";
 };
 // 완료 목록 추가
@@ -39,17 +89,8 @@ const addDone = () => {
     alert("write a task");
     return;
   }
-  const wrapper = document.createElement("div");
-  wrapper.className = "done-flexbox";
-  const newTodo = document.createElement("div");
-  newTodo.className = "done-item";
-  newTodo.appendChild(document.createTextNode(inputContent.value));
-  wrapper.appendChild(newTodo);
-  const link = document.createElement("a");
-  link.className = "delete-done";
-  link.innerHTML = `<i class="far fa-trash-alt"></i>`;
-  wrapper.appendChild(link);
-  doneList.appendChild(wrapper);
+  makeDonelist();
+  storeDone(inputContent.value);
   inputContent.value = "";
 };
 
@@ -57,12 +98,14 @@ const addDone = () => {
 const removeTodo = (e) => {
   if (e.target.parentElement.classList.contains("delete-todo")) {
     e.target.parentElement.parentElement.remove();
+    removeTodoFromLS(e.target.parentElement.parentElement);
   }
 };
 // 완료 리스트 삭제
 const removeDone = (e) => {
   if (e.target.parentElement.classList.contains("delete-done")) {
     e.target.parentElement.parentElement.remove();
+    removeDoneFromLS(e.target.parentElement.parentElement);
   }
 };
 // 투두리스트 전체 삭제
@@ -137,20 +180,48 @@ closeModalBtn.addEventListener("click", () => {
   }
 });
 
-//  투두리스트 삭제 이벤트
-todoList.addEventListener("click", removeTodo);
-// 완료 리스트 삭제 이벤트
-doneList.addEventListener("click", removeDone);
-// 전체 투두리스트 삭제
-deleteAllTodosBtn.addEventListener("click", removeAllTodos);
-// 전체 완료리스트 삭제
-deleteAllDonesBtn.addEventListener("click", removeAllDones);
+const getTasks = () => {
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  if (localStorage.getItem("dones") === null) {
+    dones = [];
+  } else {
+    dones = JSON.parse(localStorage.getItem("dones"));
+  }
+  todos.forEach((todo) => {
+    makeTodolist(todo);
+  });
+  dones.forEach((done) => {
+    makeDonelist(done);
+  });
+};
 
-// 검색 필터링 기능
-todoSearch.addEventListener("keyup", filterTodos);
+// 전체 이벤트 리스너 가져오기
 
-doneSearch.addEventListener("keyup", filterDones);
+const loadEventListeners = () => {
+  // DOM load event
+  document.addEventListener("DOMContentLoaded", getTasks);
 
-// 검색버튼 눌렀을 때
-todoSearchBtn.addEventListener("click", resetTodoSearch);
-doneSearchBtn.addEventListener("click", resetDoneSearch);
+  //  투두리스트 삭제 이벤트
+  todoList.addEventListener("click", removeTodo);
+  // 완료 리스트 삭제 이벤트
+  doneList.addEventListener("click", removeDone);
+  // 전체 투두리스트 삭제
+  deleteAllTodosBtn.addEventListener("click", removeAllTodos);
+  // 전체 완료리스트 삭제
+  deleteAllDonesBtn.addEventListener("click", removeAllDones);
+
+  // 검색 필터링 기능
+  todoSearch.addEventListener("keyup", filterTodos);
+
+  doneSearch.addEventListener("keyup", filterDones);
+
+  // 검색버튼 눌렀을 때
+  todoSearchBtn.addEventListener("click", resetTodoSearch);
+  doneSearchBtn.addEventListener("click", resetDoneSearch);
+};
+
+loadEventListeners();
