39bc9bc68edc8cabfad61c526a9fc4503f4f3459
haerangChoi <gofkdvjvl@naver.com>
2021-02-15 15:41:15 +0900

    feat:진행도에 따라 식물 성장하도록 구현

--- plant.js
+++ plant.js
@@ -1,6 +1,7 @@
 const numOfComplete = document.getElementById("completeNum");
 const numOfTotal = document.getElementById("totalNum");
 const progress = document.querySelector(".progress");
+const plantImg = document.getElementsByTagName("img")[0];
 
 const displayCnt = () => {
   numOfComplete.innerText = dones.length;
@@ -9,6 +10,16 @@ const displayCnt = () => {
     const len = Math.round(
       (dones.length / (dones.length + todos.length)) * 100
     );
+    console.log(plantImg);
+    if (len < 30) {
+      plantImg.src = "./images/one.png";
+    } else if (len < 60) {
+      plantImg.src = "./images/two.png";
+    } else if (len < 90) {
+      plantImg.src = "./images/four.png";
+    } else {
+        
+    }
     progress.innerText = `${len}%`;
     progress.style.width = `${len}%`;
   }
