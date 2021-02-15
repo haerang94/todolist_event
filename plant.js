975c8a50ee7cd85f2e348366301b77f10842a52f
haerangChoi <gofkdvjvl@naver.com>
2021-02-15 15:52:44 +0900

    feat: 랜덤 꽃 추가함

--- plant.js
+++ plant.js
@@ -9,6 +9,12 @@ const flowers = [
   "https://images.unsplash.com/photo-1464820453369-31d2c0b651af?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjV8fGZsb3dlcnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
   "https://images.unsplash.com/photo-1487139975590-b4f1dce9b035?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
   "https://images.unsplash.com/photo-1596808119772-d3ad38f5d8b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80",
+  "https://images.unsplash.com/photo-1567538737566-184fdf777891?ixid=MXwxMjA3fDB8MHxzZWFyY2h8ODF8fGZsb3dlcnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+  "https://images.unsplash.com/photo-1602931703142-3d6316a74d5c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTU1fHxmbG93ZXJ8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+  "https://images.unsplash.com/photo-1593600312294-e0652c60a26b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=282&q=80",
+  "https://images.unsplash.com/photo-1556647944-11bc0d4e0c4a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+  "https://images.unsplash.com/photo-1471899236350-e3016bf1e69e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80",
+  "https://images.unsplash.com/photo-1491246176266-6bf18143d71d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80",
 ];
 const displayCnt = () => {
   numOfComplete.innerText = dones.length;
